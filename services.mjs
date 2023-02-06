a82acb8f62cdd12bd334bdb25f515c5062f577dc
Anders Houbak Kristiansen <palantus@gmail.com>
2023-02-06 17:27:11 +0100

    Role name change

--- services.mjs
+++ services.mjs
@@ -4,7 +4,7 @@ import Query, {UNIQUE_TYPE_NAME as QueryTypeName} from "./models/query.mjs"
 
 export default async () => {
   // init
-  Role.lookupOrCreate("LD2-Admin").addPermission(["ld2.query.edit", "ld2.query.admin", "ld2.query.read", "ld2.read"], true)
+  Role.lookupOrCreate("ld2-admin").addPermission(["ld2.query.edit", "ld2.query.admin", "ld2.query.read", "ld2.read"], true)
 
   DataType.lookupOrCreate(QueryTypeName, {title: "LD2 query", permission: "ld2.query.read", api: "ld2/query", nameField: "title", acl: "r:private;w:private"})
           .init({typeModel: Query})
